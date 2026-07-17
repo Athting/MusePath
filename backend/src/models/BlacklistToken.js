803b93214e85ebb143f1c8b68a2f8b3ef565a067
Atharv R. Tingane <atharvtingane2@gmail.com>
2026-07-17 23:20:59 +0530

    updated schema

--- backend/src/models/BlacklistToken.js
+++ backend/src/models/BlacklistToken.js
@@ -2,11 +2,14 @@ import mongoose from 'mongoose'
 
 const BlacklistTokenSchema = new mongoose.Schema(
     {
-        token: { type: String, required: true, unique: true, index: true },
-        expiresAt: { type: Date, required: true, expires: 0 }
+        token: { 
+            type: String, 
+            required: true, 
+            unique: true, 
+            index: true 
+        }
     },
     { timestamps: true }
 )
 
 export const BlacklistToken = mongoose.model('BlacklistToken', BlacklistTokenSchema)
-
