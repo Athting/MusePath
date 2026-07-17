4d9d7dbfebdf182697e7aa9cc7e51fa0c2f2bf0a
Atharv R. Tingane <atharvtingane2@gmail.com>
2026-07-17 17:16:25 +0530

    app.js added

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import authRouter from './routes/auth.js'
+import apiRouter from './routes/api.js'
+
+const app = express()
+const frontendUrl = process.env.FRONTEND_URL
+
+app.use(express.json({ 
+    limit: '2mb', 
+    type: ['application/json', 'text/plain'] 
+}))
+
+app.use(cookieParser())
+
+app.use(cors({
+    origin: frontendUrl,
+    credentials: true
+}))
+
+app.use('/api/auth', authRouter)
+app.use('/api', apiRouter)
+
+export default app
